a7153a0e34bb901ea11616f7aa03ad4e86f1d74e
GlutVonSmark <33777882+GlutVonSmark@users.noreply.github.com>
2021-12-24 11:05:57 +0000

    Update allowances.sol

--- allowances.sol
+++ allowances.sol
@@ -3,25 +3,49 @@
 pragma solidity >=0.7.0 <0.9.0;
 import "./2_Owner.sol";
 
+/* TODO: save to github */
+/* TODO: deploy this to testnet and build UI for this */
 
-contract SharedWallet is Owner {
+contract Allowance is Owner {
+    
+    event AllowanceChanged(address indexed _forWho, address indexed _whoChanged, uint oldAllowance, uint _newAllowance);
+    uint private defaultAllowance = 3; 
+    mapping(address => uint) internal allowances;
 
-    event Received(address, uint);
-
-    modifier hasWallet() {
+     modifier hasWallet() {
         require(allowances[msg.sender] != 0 || msg.sender == owner , "You're not in this wallet.");
         _;
     }
-    
-    uint private defaultAllowance = 3; 
-    mapping(address => uint) private allowances;
 
+    function reduceAllowance(address _who, uint _amount) internal {
+        emit AllowanceChanged(_who, msg.sender, allowances[_who], allowances[_who] - _amount);
+        allowances[_who] -= _amount;
+    }
 
+    function addAddress(address _newAddress) public isOwner {
+        /* TODO: only if there is no allowance there */
+        allowances[_newAddress] = defaultAllowance;
+    }
+
+    function changeAllowance(address _address, uint _newAllowance) public isOwner {
+        emit AllowanceChanged(_address, msg.sender, allowances[_address], _newAllowance);
+        allowances[_address] = _newAllowance;
+    }
+
+    function getAllowance(address _address) public isOwner view returns (uint) {
+        return allowances[_address];
+    }
+
+}
+
+contract SharedWallet is Allowance {
+
+    event EtherReceived(address indexed _from, uint _amount);
+    event EtherWithdrawn(address indexed _beneficiary, uint _amount);
 
     function getBalance() public view returns (uint) {
         return address(this).balance;
     }
-
  
     function withdraw(uint _amount) public hasWallet payable {
         require(_amount <= getBalance(), "Not enought funds in the wallet");
@@ -32,27 +56,14 @@ contract SharedWallet is Owner {
         } else {
             uint allowance = allowances[msg.sender];
             require(_amount <= allowance, "You tried to withdraw more than your allowance");
+            reduceAllowance(msg.sender, _amount);
+            emit EtherWithdrawn(msg.sender, _amount);
             payable(msg.sender).transfer(_amount);
         }
         
     }
 
-    /* question...optional parameters? */
-    function addAddress(address _newAddress) public isOwner {
-        /* for now we dont care if the address is already there */
-        allowances[_newAddress] = defaultAllowance;
-    }
-
-    function changeAllowance(address _address, uint _newAllowance) public isOwner {
-        allowances[_address] = _newAllowance;
-    }
-
-    function getAllowance(address _address) public isOwner view returns (uint) {
-        return allowances[_address];
-    }
-
     receive() external payable {
-        emit Received(msg.sender, msg.value);
+        emit EtherReceived(msg.sender, msg.value);
     }
-
 }
